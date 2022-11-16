674fbb16eadba6486073abbb1878350c6b23c056
M Katherine <86696439+kathBD@users.noreply.github.com>
2022-11-16 16:33:38 -0500

    estados react

--- Frameworks_librerias_js/demo-react/src/Form.js
+++ Frameworks_librerias_js/demo-react/src/Form.js
@@ -1,14 +1,27 @@
+
 import React from 'react'
 
 export default function form (props) {
+
+//react hook
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [quantity, setQuantity] = React.useState(0);
+
   const{movie}= props;
 
   return (
     <form>
         <h3>{movie.name}</h3>
-        <button>-</button>
-        <button>+</button>
+     
+        <button type='button'onClick={()=>setQuantity(quantity -1)} disabled={quantity <= 0}>-</button>
+        {quantity}
+        <button type='button' onClick={()=>setQuantity(quantity +1)} disabled={quantity >= movie.available}>+</button>
     </form>
+
+
+
+
   );
 }
 
