9fddb3c68844dd27a55e05b17d415d613be28b0e
M Katherine <86696439+kathBD@users.noreply.github.com>
2022-11-17 11:47:01 -0500

    svelte-demo

---
+++ Frameworks_librerias_js/svelte/myapp/src/Form.svelte
@@ -0,0 +1,34 @@
+<script>
+ const movies = [
+              {
+                name: "Batman",
+                available: 5,
+                quantity: 0,
+              },
+              {
+                name: "Hombre del Norte",
+                available: 4,
+                quantity: 0,
+              },
+              {
+                name: "Capitan America",
+                available: 10,
+                quantity: 0,
+              },
+            ];
+
+</script>
+
+{#each movies as movie}
+<form>
+  <h3>{movie.name}</h3>
+  <button type="button" on:click={()=>movie.quantity -=1} disabled={movie.quantity <=0}>
+    -
+  </button>
+  {movie.quantity}  
+  <button type="button" on:click={()=>movie.quantity +=1} disabled={movie.quantity >= movie.available}>
+    +
+  </button>
+
+ </form>
+{/each}
